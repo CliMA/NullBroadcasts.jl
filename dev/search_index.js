7ffaf36165efd239430f219d0a2c227da485a66b
Documenter.jl <documenter@juliadocs.github.io>
2026-07-02 21:16:39 +0000

    build based on 440fb39

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"references/#References","page":"References","text":"","title":"References"},{"category":"section","location":"api/#API","page":"API","text":"","title":"API"},{"category":"section","location":"#NullBroadcasts.jl","page":"Home","text":"Under construction. Please see the readme for the latest documentation.","title":"NullBroadcasts.jl"}]
+}
---
+++ previews/PR6/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"references/#References","page":"References","title":"References","text":"","category":"section"},{"location":"references/","page":"References","title":"References","text":"","category":"page"},{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#NullBroadcasts.jl","page":"Home","title":"NullBroadcasts.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Under construction. Please see the readme for the latest documentation.","category":"page"}]
+}
